e0b45a336b4384120c983c74ebee00a85fdf4049
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-26 09:11:56 +0100

    Introduction to java. Started variable

---
+++ firstapp/src/mainpackagedemo/Variables.java
@@ -0,0 +1,13 @@
+package mainpackagedemo;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "Nerdster Akinz";
+		System.out.println("My name is "+name);
+
+	}
+
+}
