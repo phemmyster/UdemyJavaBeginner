6bbeb695e075e703411f2ebf650cd9e11854e0cd
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-26 20:20:14 +0100

    conversion from one variable to another

--- firstapp/src/mainpackagedemo/Inputs.java
+++ firstapp/src/mainpackagedemo/Inputs.java
@@ -1,9 +1,29 @@
 package mainpackagedemo;
 
+import java.util.Scanner;
+
 public class Inputs {
+	static Scanner reader1 = new Scanner(System.in);
+	static Scanner reader = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		System.out.println("Enter your full name:");
+		String name = reader.nextLine();
+		// int age = 34;
+		System.out.println("Enter your age:");
+		int age = reader.nextInt();
+		// double Salary = 1200000.50;
+		System.out.println("Enter your salary:");
+		double Salary = reader.nextDouble();
+		System.out.println("Enter your Gender:");
+		String input = reader1.nextLine();
+		char Gender = input.charAt(0);
+		
+		
+		System.out.println("My name is "+name +" , I am "+ age +" years old");
+		System.out.println("My gender is "+ Gender);
+		System.out.println("My salary is "+ Salary);
 
 	}
 
--- firstapp/src/mainpackagedemo/Variables.java
+++ firstapp/src/mainpackagedemo/Variables.java
@@ -6,7 +6,16 @@ public class Variables {
 		// TODO Auto-generated method stub
 		
 		String name = "Nerdster Akinz";
-		System.out.println("My name is "+name);
+		int age = 34;
+		double Salary = 1200000.50;
+		char Gender = 'M';
+		
+		
+		System.out.println("My name is "+name +" , I am "+ age +" years old");
+		System.out.println("My gender is "+ Gender);
+		System.out.println("My salary is "+ Salary);
+		
+		
 
 	}
 
