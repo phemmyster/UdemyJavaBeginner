7bca266c97d6cffb0b3b66c5a9c749307430b384
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-27 08:50:43 +0100

    various conversion types

--- firstapp/src/mainpackagedemo/convert.java
+++ firstapp/src/mainpackagedemo/convert.java
@@ -6,14 +6,38 @@ public class convert {
 		// TODO Auto-generated method stub
 		int age = 27;
 		double salary = 3.54;
+		boolean isTrue = true;
+		String rate = "3";
+		String rate2 ="7.34";
 		
+		// to convert double to string
 		String NewSalary = String.valueOf(salary);
 		System.out.println(NewSalary);
 		
-		// to convert to string
+		// to convert into to string
 		String NewAge = String.valueOf(age);
 		System.out.println(NewAge);
 
+		// to convert boolean to string
+		String NewIsTrue = String.valueOf(isTrue);
+		System.out.println(NewIsTrue);
+				
+		// convert string to integer
+		int NewRate = Integer.parseInt(rate);
+		
+		System.out.println("New age 1 is "+NewRate);
+		int NewRate2 = Integer.valueOf(rate);
+		System.out.println("New age 2 is "+NewRate2);
+		
+		//convert to double
+		
+		double Newrate = Double.parseDouble(rate2);
+		
+		System.out.println("New rate 1 is "+Newrate);
+		double Newrate2 = Double.valueOf(rate2);
+		System.out.println("New rate 2 is "+Newrate2);
+		
+		
 	}
 
 }
