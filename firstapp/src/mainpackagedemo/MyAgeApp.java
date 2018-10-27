61f9d6e356d5a7432a9ee456f89168e80a006b8d
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-27 17:36:33 +0100

    Simple app to calculate age.

--- firstapp/src/mainpackagedemo/MyAgeApp.java
+++ firstapp/src/mainpackagedemo/MyAgeApp.java
@@ -1,10 +1,18 @@
 package mainpackagedemo;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class MyAgeApp {
 
+	static Scanner reader = new Scanner(System.in);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Enter DOB:");
+		int DOB = reader.nextInt();
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+int age = year- DOB;
+System.out.println("You are "+age+ " years old!");
 	}
 
 }
