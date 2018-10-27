d773d5dd6422953f37892ffb4292baa1a8e0fd65
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-27 20:12:10 +0100

    More on truth table.

--- firstapp/src/mainpackagedemo/Logic.java
+++ firstapp/src/mainpackagedemo/Logic.java
@@ -10,7 +10,15 @@ public class Logic {
 		 System.out.println(3==4);
 		 System.out.println(3==3);
 		 System.out.println(3!=3);
-
+		 
+		 // and && or ||
+		 /*
+		  * && will be true only work when both conditions are true
+		  * || will be true if only one of the conditions is true
+		  */
+		 System.out.println("Combining two operators");
+		 System.out.println( (3>4) && (2== 4));
+		 System.out.println( (3>1) && (4<1));
 	}
 
 }
