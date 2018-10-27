1d02302ed8554c35ef767d2f05d1b984a5c9a0b6
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-27 17:21:12 +0100

    Pay attention to ++ and --- before and after number

--- firstapp/src/mainpackagedemo/operation.java
+++ firstapp/src/mainpackagedemo/operation.java
@@ -4,12 +4,15 @@ public class operation {
 
 	public static void main(String[] args) {
 		/**
+		 * 
 		 * order of operation
+		 * (bracket)
+		 * (-- or ++ before)
 		 * power ^
 		 * multiplication * and division /
 		 * addition and subtraction
 		 * equal =
-		 * 
+		 * (-- or ++ after)
 		 * */
 		
 		int n1 = 5, n2 = 10, n3 = 2;
@@ -22,6 +25,18 @@ public class operation {
 		System.out.println("Sum 1: " + sum1);
 		System.out.println("Sum 2: " + sum2);
 		System.out.println("Sum 3: " + sum3);
+		
+		
+		System.out.println("&&&&&&&&&&&&&&&&&&&&");
+		// ++n = n++ ->n+1
+		// --n = n-- ->n-1
+		int sum4 =(++n1 + n2) * n3;
+		System.out.println("Sum 4: " + sum4);
+		System.out.println("n1 is "+n1);
+		int sum5 =(n1++ + n2) * n3;
+		System.out.println("Sum 5: " + sum5);
+		System.out.println("n1 is "+n1);
+		
 	}
 
 }
