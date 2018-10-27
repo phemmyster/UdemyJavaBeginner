a1fe6cd2593d811afd2841cc594fba24b26955e9
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-27 06:56:49 +0100

    conversion to string

---
+++ firstapp/src/mainpackagedemo/convert.java
@@ -0,0 +1,19 @@
+package mainpackagedemo;
+
+public class convert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 27;
+		double salary = 3.54;
+		
+		String NewSalary = String.valueOf(salary);
+		System.out.println(NewSalary);
+		
+		// to convert to string
+		String NewAge = String.valueOf(age);
+		System.out.println(NewAge);
+
+	}
+
+}
