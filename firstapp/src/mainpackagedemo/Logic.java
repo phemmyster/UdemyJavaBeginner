87bfd581ca12c43316daa44bec270dcbd0f5c63d
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-27 19:58:45 +0100

    logic lecture.

---
+++ firstapp/src/mainpackagedemo/Logic.java
@@ -0,0 +1,16 @@
+package mainpackagedemo;
+
+public class Logic {
+
+	public static void main(String[] args) {
+		 System.out.println( 3>4);
+		 System.out.println(3>=4);
+		 System.out.println(3<4);
+		 System.out.println(3<=4);
+		 System.out.println(3==4);
+		 System.out.println(3==3);
+		 System.out.println(3!=3);
+
+	}
+
+}
