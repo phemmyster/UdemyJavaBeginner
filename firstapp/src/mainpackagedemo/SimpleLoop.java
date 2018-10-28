1f04ba56bc535ea8d7942e3370a582cbbed8a754
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-10-28 19:33:13 +0100

    sample of a nested loop

--- firstapp/src/mainpackagedemo/SimpleLoop.java
+++ firstapp/src/mainpackagedemo/SimpleLoop.java
@@ -12,9 +12,17 @@ public class SimpleLoop {
 		System.out.println("Hello");
 		
 		*/
-		for (int i = 0; i < 5; i++) {
+		/*for (int i = 0; i < 5; i++) {
 			System.out.println("Count: "+i);
 			System.out.println("Hello");
+		}*/
+		System.out.println("Number between 1 and 100 divisible by 4 are: ");
+		for (int i = 0; i < 100; i=i+2) {
+			//System.out.println("Count: "+i);
+			if (i % 4 == 0) {
+				System.out.println(i);
+			}
+			
 		}
 
 	}
