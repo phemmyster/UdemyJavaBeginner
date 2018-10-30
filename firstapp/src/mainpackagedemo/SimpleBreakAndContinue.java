72905e6925005f0b0686a30e8518c5ed5f306439
phemmyster <phemmyster@yahoo.com>
2018-10-30 14:10:02 +0100

    Understanding the concept of break and continue

--- firstapp/src/mainpackagedemo/SimpleBreakAndContinue.java
+++ firstapp/src/mainpackagedemo/SimpleBreakAndContinue.java
@@ -2,8 +2,21 @@ package mainpackagedemo;
 
 public class SimpleBreakAndContinue {
 
+	/*
+	 * continue - will move on with the block break - will stop the block of
+	 * code from continuing
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//System.out.println("Numbers between 1 to 100 divisible by 4 are:");
+		for (int i = 1; i <= 100; i++) {
+				if (i % 4 == 0){
+					//continue;
+					  break;
+				}
+		System.out.println("count: " + i);
+		System.out.println("count is printed");
+		}
 
 	}
 
