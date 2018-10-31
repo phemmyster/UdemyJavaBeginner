b26ef9eb35e6cfbf49dc488ac94feb658afb3365
phemmyster <phemmyster@yahoo.com>
2018-10-31 14:46:48 +0100

    sample question on array

--- firstapp/src/mainpackagedemo/One_D_Array.java
+++ firstapp/src/mainpackagedemo/One_D_Array.java
@@ -6,12 +6,16 @@ public class One_D_Array {
 		// TODO Auto-generated method stub
 		
 		String[] jobs = new String[5];
-		jobs[0] = "Account";
-		jobs[1] = "Marketer";
+		jobs[0] = "Tester";
+		jobs[1] = "QA";
 		jobs[2] = "Programmer";
-		jobs[3] = "Account";
-		jobs[4] = "Account";
+		jobs[3] = "Manager";
+		jobs[4] = "Admin";
 		//String[] jobs = {};
+		
+		for (int i= 0; i<jobs.length; i++) {
+			System.out.println("Jobs available are: "+jobs[i]);
+		}
 
 	}
 
