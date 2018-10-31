c8d8111f4f508d8928adc2898faadbae35513815
phemmyster <phemmyster@yahoo.com>
2018-10-31 14:02:14 +0100

    Added this from skolPC to master

---
+++ firstapp/src/mainpackagedemo/One_D_Array.java
@@ -0,0 +1,18 @@
+package mainpackagedemo;
+
+public class One_D_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] jobs = new String[5];
+		jobs[0] = "Account";
+		jobs[1] = "Marketer";
+		jobs[2] = "Programmer";
+		jobs[3] = "Account";
+		jobs[4] = "Account";
+		//String[] jobs = {};
+
+	}
+
+}
