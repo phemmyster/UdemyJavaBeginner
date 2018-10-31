7106b854d3d4c6c2482e7df799cf2c08e0bca69f
phemmyster <phemmyster@yahoo.com>
2018-10-31 15:59:04 +0100

    being trying to save this since

--- firstapp/src/mainpackagedemo/MyArray.java
+++ firstapp/src/mainpackagedemo/MyArray.java
@@ -4,6 +4,17 @@ public class MyArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] jobs = new String[5];
+		jobs[0] = "Tester";
+		jobs[1] = "QA";
+		jobs[2] = "Programmer";
+		jobs[3] = "Manager";
+		jobs[4] = "Admin";
+		//String[] jobs = {};
+		
+		for (int i= 0; i<jobs.length; i++) {
+			System.out.println("Jobs available are: "+jobs[i]);
+		}
 
 	}
 
