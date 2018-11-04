d1f7528bb6e12caff9c0c919b327eaca88c0340f
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-11-04 17:03:09 +0100

    First part of array

---
+++ firstapp/src/mainpackagedemo/MyArray.java
@@ -0,0 +1,26 @@
+package mainpackagedemo;
+
+import java.util.Scanner;
+
+public class MyArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] jobs = new String[5];
+		
+		//String[] jobs = {"Admin", "Manager", "Tester","QA","Developer"} ;
+		// want the person to enter their jobs
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter the jobs: ");
+		for (int i = 0; i < args.length; i++) {
+			jobs[i] = reader.nextLine();
+		System.out.println("print job");
+		
+//		System.out.println("The various jobs available are:");
+//		for (int i = 0; i < jobs.length; i++) {
+//			System.out.println(jobs[i]);
+//		}
+		}
+	}
+
+}
