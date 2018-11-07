9fedd55bdef96e74ddd4caccf30a771d6074eea6
phemmyster <phemmyster@yahoo.com>
2018-11-07 12:52:51 +0100

    String split of word

--- firstapp/src/mainpackagedemo/StringFile.java
+++ firstapp/src/mainpackagedemo/StringFile.java
@@ -11,9 +11,28 @@ public class StringFile {
 		
 		System.out.println("To print out the char of my name:");
 		for (int i = 0; i < name.length(); i++) {
-			System.out.println(name.charAt(i));
+			//System.out.println(name.charAt(i));
+			//("To print out the char of my name in one line:");
+			System.out.print(name.charAt(i));
+		}
+		System.out.println("To prevent name in reverse");
+		for (int i = name.length()-1; i>=0 ; i--) {
+				//System.out.println(name.charAt(i));
+				//("To print out the char of my name in one line:");
+				System.out.print(name.charAt(i));
+		}
+		// splitting of a sentence
+		System.out.println("Print word:");
+		String message = "Nerdster is the best coder";
+		String[] splitMsg = message.split(" ");
+		
+		for (int i = 0; i < splitMsg.length; i++) {
+			System.out.println(splitMsg[i]);
 			
 		}
-	}
+		
+		
+	
 
+}
 }
