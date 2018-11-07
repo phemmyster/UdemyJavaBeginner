75c4c21cb584b9d539a0c4dfeacd3b3e3bc8fec8
phemmyster <phemmyster@yahoo.com>
2018-11-07 07:16:06 +0100

    print out the character of a given string

--- firstapp/src/mainpackagedemo/StringFile.java
+++ firstapp/src/mainpackagedemo/StringFile.java
@@ -8,6 +8,12 @@ public class StringFile {
 		System.out.println("Length of character in name: "+name.length());
 		System.out.println("First character of my name: "+name.charAt(0));
 		System.out.println("Last character of my name: "+name.charAt(name.length()-1));
+		
+		System.out.println("To print out the char of my name:");
+		for (int i = 0; i < name.length(); i++) {
+			System.out.println(name.charAt(i));
+			
+		}
 	}
 
 }
