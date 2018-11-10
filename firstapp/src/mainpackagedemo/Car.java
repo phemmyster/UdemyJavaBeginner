1c5eaaa993f92529c34b916ce50dc1e11b083753
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-11-10 16:47:42 +0100

    default constructor and constructor initiallised with parameter!

--- firstapp/src/mainpackagedemo/Car.java
+++ firstapp/src/mainpackagedemo/Car.java
@@ -8,6 +8,21 @@ public class Car {
 	double MilesDrive;
 	String Owner;
 	
+	//default constructor
+	public Car(){
+		System.out.println("Default constructor!");
+	}
+	
+	public Car(String Type,int Model,double Price,double MilesDrive,String Owner){
+		this.Type= Type;
+		this.Model = Model;
+		this.Price= Price;
+		this.MilesDrive = MilesDrive;
+		this.Owner = Owner;
+		System.out.println("constructor 2 is created!");
+	}
+
+	
 	double GetPrice(){
 		double NewPrice = Price - (MilesDrive* 100);
 		return NewPrice;
--- firstapp/src/mainpackagedemo/CarMain.java
+++ firstapp/src/mainpackagedemo/CarMain.java
@@ -5,17 +5,20 @@ public class CarMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Car car1 = new Car();
+		Car car1 = new Car(); // this wnt work again cuz of default constructor
+		car1.Type ="BWM";
 		car1.MilesDrive = 5;
 		car1.Model = 2016;
 		car1.Owner = "Phemmy";
 		car1.Price = 10000;
 		
-		System.out.println("Car price is given as:"+car1.GetPrice());
+		System.out.println("Car1 price is given as:"+car1.GetPrice());
 		
-		Car car2 = new Car();
-		System.out.println("Car price is given as:"+car2.GetPrice());
+		//Car car2 = new Car();
+		//System.out.println("Car2 price is given as:"+car2.GetPrice());
 		
+		Car car3 = new Car("Honda",2019,100000,10,"Niffy");
+		System.out.println("Car3 price is given as:"+car3.GetPrice());
 
 	}
 
