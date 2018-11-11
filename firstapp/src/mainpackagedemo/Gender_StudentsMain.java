e37fea8934bb528eef346c10b7a569d6749a2de8
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-11-11 18:54:27 +0100

    still on enum

--- firstapp/src/mainpackagedemo/Gender_StudentsMain.java
+++ firstapp/src/mainpackagedemo/Gender_StudentsMain.java
@@ -6,6 +6,8 @@ public class Gender_StudentsMain {
 		// TODO Auto-generated method stub
 		Gender_Students student1 = new Gender_Students("Phemmy", Gender_Enum.Male);
 		System.out.println("Gender: "+ student1.gender);
+		System.out.println("Name: "+ student1.FirstName);
+		
 
 	}
 
