809acfe1041361bf0d159439cab83b37842ac8ba
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-11-11 18:09:08 +0100

    can implement more than one interface but can only extend one class

--- firstapp/src/mainpackagedemo/Operations_Interface.java
+++ firstapp/src/mainpackagedemo/Operations_Interface.java
@@ -1,5 +1,6 @@
 package mainpackagedemo;
 
+// can implement more than one interface but can only extend one class
 public interface Operations_Interface {
 	int sum(int n1, int n2);
 	int sub(int n1, int n2);
