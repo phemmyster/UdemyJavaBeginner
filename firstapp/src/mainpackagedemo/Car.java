2274dd61f0bd6d5b1a9865d69f21de82753d4c40
phemmyster <phemmyster@yahoo.com>
2018-11-12 13:59:29 +0100

    fixed the error

--- firstapp/src/mainpackagedemo/Car.java
+++ firstapp/src/mainpackagedemo/Car.java
@@ -1,12 +1,33 @@
 package mainpackagedemo;
 
-public class Car {
+//abstract public class Car {  // if u use abstract it wnt be acesseible
+	public class Car {
 // CarMain is the tester for this class
-	String Type;
-	int Model;
-	double Price;
-	double MilesDrive;
-	String Owner;
+	//private String Type;
+	public String Type;
+	//private int Model;
+	public int Model;
+	//private double Price;
+	public double Price;
+	//private double MilesDrive;
+	public double MilesDrive;
+	static String Owner; // is common to all
+	Owners owners;
+	
+	
+	public Car(String Type,int Model,double Price,double MilesDrive){
+		this.Type= Type;
+		this.Model = Model;
+		this.Price= Price;
+		this.MilesDrive = MilesDrive;
+		this.Owner = Owner;
+		 owners = new Owners();
+		 owners.FirstName = "Phemmy";
+		 owners.LastName="Akinz";
+		 
+		System.out.println("constructor 2 is created!");
+	}
+
 	
 	public Car(){   // constructor
 		System.out.println("class created in constructor!");
