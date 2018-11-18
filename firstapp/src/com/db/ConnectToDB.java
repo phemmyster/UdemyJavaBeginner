c6512dee43875b4f8b21a44bb29f0d23b640effe
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-11-18 13:15:08 +0100

    modified version of the simple add to db

--- firstapp/src/com/db/ConnectToDB.java
+++ firstapp/src/com/db/ConnectToDB.java
@@ -1,5 +1,6 @@
 package com.db;
 import java.sql.*;
+import java.util.Scanner;
 public class ConnectToDB {
 
 	public static void main(String[] args) {
@@ -10,10 +11,33 @@ public class ConnectToDB {
 			c = DriverManager.getConnection("jdbc:sqlite:info.db");
 			System.out.println("Connection Successful!");
 			// add users
-			String SQLAdd = "insert into admins(user_name,password) values('nerdster','12345')";
-			Statement stmt = c.createStatement();
-			stmt.executeUpdate(SQLAdd);
-			System.out.println("Added to db");
+			System.out.println("Use operation\n1- Add new user\n");
+			Scanner reader = new Scanner(System.in);
+			int processIndex = reader.nextInt();
+			
+			switch (processIndex) {
+			case 1:// add
+				Scanner username = new Scanner(System.in);
+				Scanner psd = new Scanner(System.in);
+				
+				System.out.print("Enter user name: ");
+				String user_name = username.nextLine();
+				
+				System.out.print("Enter password: ");
+				String password = psd.nextLine();
+				
+			//	String SQLAdd = "insert into admins(user_name,password) values('debby','queenBee23')";
+				String SQLAdd = "insert into admins(user_name,password) values('"+user_name+"','"+password+"')";
+				Statement stmt = c.createStatement();
+				stmt.executeUpdate(SQLAdd);
+				System.out.println("Added to db");
+				
+				break;
+
+			default:
+				break;
+			}
+			
 			
 		} catch (Exception e) {
 			// TODO: handle exception
