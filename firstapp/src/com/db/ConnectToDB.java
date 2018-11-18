486b55f4af9da245ecd37101a1d1cf9a2f2af44a
Akinfenwa Phemmy <phemmyster@yahoo.com>
2018-11-18 12:00:14 +0100

    first attempt

--- firstapp/src/com/db/ConnectToDB.java
+++ firstapp/src/com/db/ConnectToDB.java
@@ -9,11 +9,18 @@ public class ConnectToDB {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:info.db");
 			System.out.println("Connection Successful!");
+			// add users
+			String SQLAdd = "insert into admins(user_name,password) values('nerdster','12345')";
+			Statement stmt = c.createStatement();
+			stmt.executeUpdate(SQLAdd);
+			System.out.println("Added to db");
+			
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println("Connection UnSuccessful!");
+			System.out.println("Cannot Connect!");
 			System.exit(0);
 		}
+		
 
 	}
 
